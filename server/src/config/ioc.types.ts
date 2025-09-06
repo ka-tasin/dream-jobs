f92164ec268c4bb68db922784779b132564b4944
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2025-09-06 19:21:26 +0600

    user folders started, inversify code starte

---
+++ server/src/config/ioc.types.ts
@@ -0,0 +1,7 @@
+const TYPES = {
+  UserController: Symbol.for("UserController"),
+  IUserRepository: Symbol.for("IUserRepository"),
+  IUserServices: Symbol.for("IUserServices"),
+
+  IUnitOfWork: Symbol.for("IUnitOfWork"),
+};
