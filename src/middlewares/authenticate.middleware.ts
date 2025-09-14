acf6f89e0f921037dd7726c9a72f33e58fff43d1
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2025-09-14 17:36:58 +0600

    added authenticate middleware

---
+++ src/middlewares/authenticate.middleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { inject, injectable } from "inversify";
+import { TYPES } from "../config/ioc.types";
+import { IUnitOfService } from "../services/interfaces/iunitOf.service";
+
+@injectable()
+export default class AuthMiddleware {
+  constructor(
+    @inject(TYPES.IUnitOfService) private unitOfService: IUnitOfService
+  ) {}
+
+  handle = (req: Request, res: Response, next: NextFunction): void => {
+    const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
+    if (!token) {
+      res.status(401).json({ message: "Token missing" });
+      return;
+    }
+
+    const payload = this.unitOfService.User.verifyToken(token);
+    if (!payload) {
+      res.status(401).json({ message: "Invalid token" });
+      return;
+    }
+
+    (req as any).user = payload;
+    next();
+  };
+}
