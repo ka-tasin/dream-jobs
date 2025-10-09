411526e7f898cb0a0af3315fe11028d131224166
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2025-10-09 12:36:48 +0600

    application router added to routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import accountRouter from "./account.routes";
 import jobRouter from "./job.routes";
 import userRouter from "./user.routes";
+import applicationRouter from "./application.routes";
 
 const routes = Router();
 
 routes.use("/auth", accountRouter);
 routes.use("/jobs", jobRouter);
 routes.use("/users", userRouter);
+routes.use("/applications", applicationRouter);
 
 export default routes;
