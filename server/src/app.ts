59b7ff120867953b18570068a9104f1a296db5a9
ka-tasin <kausarahmadtasinkat@gmail.com>
2026-07-18 11:25:35 +0600

    add Secure HTTP-Only Cookies

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,15 @@ import express, { Express, Request, Response } from "express";
 import routes from "./routes/index.js";
 import cors from "cors";
 import { errorHandler } from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
+
+
 app.use("/api/v1", routes);
 
 app.get("/", (req: Request, res: Response) => {
