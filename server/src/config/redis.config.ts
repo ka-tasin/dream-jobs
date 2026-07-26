efbba6774c9002d3fe315fae53da286f88f32c77
ka-tasin <kausarahmadtasinkat@gmail.com>
2026-07-26 15:24:55 +0600

    fix: redis url

--- server/src/config/redis.config.ts
+++ server/src/config/redis.config.ts
@@ -1,6 +1,10 @@
 import { ConnectionOptions } from "bullmq";
 
-export const redisConnectionOptions: ConnectionOptions = {
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: Number(process.env.REDIS_PORT) || 6379,
-};
+const redisUrl = process.env.REDIS_URL;
+
+export const redisConnectionOptions: ConnectionOptions = redisUrl
+  ? { url: redisUrl }
+  : {
+      host: process.env.REDIS_HOST || "127.0.0.1",
+      port: Number(process.env.REDIS_PORT) || 6379,
+    };
